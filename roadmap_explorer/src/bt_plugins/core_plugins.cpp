54e3f023383a5070d51c96ccaef2263f5e2381a5
suchetanrs <79915569+suchetanrs@users.noreply.github.com>
2025-07-06 23:45:32 +0530

    BT Node Pluginlib (#5)

---
+++ roadmap_explorer/src/bt_plugins/core_plugins.cpp
@@ -0,0 +1,45 @@
+#include "roadmap_explorer/bt_plugins/core_plugins.hpp"
+#include <pluginlib/class_list_macros.hpp>
+
+namespace roadmap_explorer
+{
+    class LogIterationBT : public BT::SyncActionNode
+    {
+    public:
+        LogIterationBT(
+            const std::string &name, const BT::NodeConfiguration &config)
+            : BT::SyncActionNode(name, config)
+        {
+            LOG_INFO("LogIterationBT Constructor");
+        }
+
+        BT::NodeStatus tick() override
+        {
+            EventLoggerInstance.incrementPlanningCount();
+            LOG_FLOW("MODULE LogIterationBT");
+            return BT::NodeStatus::SUCCESS;
+        }
+    };
+
+    LogIteration::LogIteration()
+    {
+    }
+
+    LogIteration::~LogIteration()
+    {
+    }
+
+    void LogIteration::registerNodes(BT::BehaviorTreeFactory &factory)
+    {
+        BT::NodeBuilder builder_log_iteration =
+            [&](const std::string &name, const BT::NodeConfiguration &config)
+        {
+            return std::make_unique<LogIterationBT>(name, config);
+        };
+        factory.registerBuilder<LogIterationBT>("LogIterationBT", builder_log_iteration);
+    }
+}
+
+PLUGINLIB_EXPORT_CLASS(
+    roadmap_explorer::LogIteration,
+    roadmap_explorer::BTPlugin)
